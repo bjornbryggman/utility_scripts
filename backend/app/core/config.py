a8ae79d5b56ff4367df950aef8beb458d62bed5f
Björn Gunnar Bryggman <59357853+bjornbryggman@users.noreply.github.com>
2024-08-06 15:15:35 +0200

    Cleaned out irrelevant files related to EU4.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,8 +5,7 @@ Provides configuration classes for managing directory paths used in the project.
 
 This module defines several configuration classes that centralize the management
 of directory paths used throughout the project. Each class encapsulates paths
-related to specific functionalities, such as geography data, scaling operations,
-and LLM prompts.
+related to specific functionalities, such as LLM prompts.
 """
 
 from pathlib import Path
@@ -79,63 +78,6 @@ class GeographyConfig:
         # Text files containing weather & terrain information.
         self.CLIMATE_TXT = self.base_config.INPUT_DIR / "map" / "climate.txt"
         self.TERRAIN_TXT = self.base_config.INPUT_DIR / "map" / "terrain.txt"
-
-
-class ScalingConfig:
-    """
-    Configuration class for directory paths used in the 'scaling_script.py'.
-
-    Attributes:
-    ----------
-        - WORKING_DIR_DDS (Path):
-        - WORKING_DIR_DDS_TO_PNG (Path):
-        - WORKING_DIR_DDS_4K (Path):
-        - WORKING_DIR_DDS_2k (Path):
-
-        - WORKING_DIR_TGA (Path):
-        - WORKING_DIR_TGA_TO_PNG (Path):
-        - WORKING_DIR_TGA_4K (Path):
-        - WORKING_DIR_TGA_2k (Path):
-
-        - TEXT_COMPARISON_ORIGINAL_DIR (Path): Path to the directory containing original files
-            for scaling factor comparison.
-        - TEXT_COMPARISON_DIR_2K (Path): Path to the directory containing 2K files
-            for scaling factor comparison.
-        - TEXT_COMPARISON_DIR_4K (Path): Path to the directory containing 4K files
-            for scaling factor comparison.
-
-        - OUTPUT_DIR_2K (Path): Path to the output directory for 2K files.
-        - OUTPUT_DIR_4K (Path): Path to the output directory for 4K files.
-    """
-
-    def __init__(self) -> None:
-        "Initializes the ScalingConfig."
-        # Create an instance of the BaseConfig class.
-        self.base_config = BaseConfig()
-
-        # Working directories.
-        # DDS files.
-        self.WORKING_DIR_DDS = self.base_config.ROOT_PATH / "resources" / "working_directory" / "dds"
-        self.WORKING_DIR_DDS_TO_PNG = self.WORKING_DIR_DDS / "dds_png"
-        self.WORKING_DIR_DDS_4K = self.WORKING_DIR_DDS / "dds_png_4k"
-        self.WORKING_DIR_DDS_2K = self.WORKING_DIR_DDS / "dds_png_2k"
-
-        # TGA files.
-        self.WORKING_DIR_TGA = self.base_config.ROOT_PATH / "resources" / "working_directory" / "tga"
-        self.WORKING_DIR_TGA_TO_PNG = self.WORKING_DIR_TGA / "tga_png"
-        self.WORKING_DIR_TGA_4K = self.WORKING_DIR_TGA / "tga_png_4k"
-        self.WORKING_DIR_TGA_2K = self.WORKING_DIR_TGA / "tga_png_2k"
-
-        # Text directories.
-        self.TEXT_COMPARISON_DIR = self.base_config.ROOT_PATH / "resources" / "text_comparison_directory"
-        self.TEXT_COMPARISON_ORIGINAL_DIR = self.TEXT_COMPARISON_DIR / "text_files_original"
-        self.TEXT_COMPARISON_DIR_4K = self.TEXT_COMPARISON_DIR / "text_files_4k"
-        self.TEXT_COMPARISON_DIR_2K = self.TEXT_COMPARISON_DIR / "text_files_2k"
-
-        # Output directories.
-        self.OUTPUT_DIR = self.base_config.ROOT_PATH / "resources" / "output_directory"
-        self.OUTPUT_DIR_4K = self.OUTPUT_DIR / "output_4k"
-        self.OUTPUT_DIR_2K = self.OUTPUT_DIR / "output_2k"
 
 
 class PromptConfig:
